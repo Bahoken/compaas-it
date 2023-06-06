3a2121b104ce9968a97efb7f8e774671140855bc
romald <romaldimben@gmail.com>
2023-06-06 10:08:49 +0200

    first commit

---
+++ countdown.js
@@ -0,0 +1,32 @@
+// Set the countdown date (YYYY-MM-DD)
+const countdownDate = new Date("2023-07-30").getTime();
+
+// Update the countdown every 1 second
+const countdown = setInterval(() => {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  const distance = countdownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  document.getElementById("days").textContent = days.toString().padStart(2, "0");
+  document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
+  document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
+  document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+
+  // If the countdown is finished, clear the interval
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("days").textContent = "00";
+    document.getElementById("hours").textContent = "00";
+    document.getElementById("minutes").textContent = "00";
+    document.getElementById("seconds").textContent = "00";
+  }
+}, 1000);
